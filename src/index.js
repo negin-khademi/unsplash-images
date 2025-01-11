b36f8ff42a9e15da3d0d6216c9bd9a7bfcc052c2
Negin <negin.khademi@gmail.com>
2025-01-11 19:04:53 +0330

    context hook (useContext)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './index.css';
 
 import App from './App';
+import { AppProvider } from './context';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
