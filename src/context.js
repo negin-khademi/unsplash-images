4b29d21fdb52cc41190316121684dcb7e1ef2a77
Negin <negin.khademi@gmail.com>
2025-01-11 22:49:19 +0330

    dark theme add-remove class

--- src/context.js
+++ src/context.js
@@ -7,8 +7,11 @@ export const AppProvider = ({ children }) => {
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
-    console.log(newDarkTheme, isDarkTheme);
     setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme', newDarkTheme);
+    // The toggle() method adds a class to the element if it does not have it, or removes it if it does. In this case, it adds the dark-theme class if isDarkTheme is true, and removes it if isDarkTheme is false.
+    console.log(body);
   };
 
   return <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>{children}</AppContext.Provider>;
