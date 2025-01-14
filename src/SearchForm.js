e3fdfe6d59d466b0a4d0150bc7746c0f6090a8e8
Negin <negin.khademi@gmail.com>
2025-01-14 16:57:19 +0330

    search form

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,5 +1,21 @@
 const SearchForm = () => {
-  return <h1>SearchForm</h1>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input type="text" className="form-input search-input" name="search" placeholder="cat" />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
