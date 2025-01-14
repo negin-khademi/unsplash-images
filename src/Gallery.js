715a22f54d7c536d13835696278a0843dfa78098
Negin <negin.khademi@gmail.com>
2025-01-14 21:02:31 +0330

    setup query

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,4 +1,17 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const url = 'https://api.unsplash.com/search/photos?client_id=BXJzyeID_-P3B04-R4akiRpO855PDUba-YbZ6o8ZEpA&query=cat';
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  console.log(response);
   return <h1>Gallery</h1>;
 };
 
