fb9de28407b8607a2a50e1aad028ed012bc61aa6
Negin <negin.khademi@gmail.com>
2025-01-14 20:52:32 +0330

    use packages in index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import App from './App';
 import { AppProvider } from './context';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </AppProvider>
   </React.StrictMode>
 );
